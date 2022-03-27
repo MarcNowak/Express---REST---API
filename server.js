737259a177e64c091a775e920b89766ca1b479e3
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-03-27 16:54:38 +0200

    Fix 29.6 TASK - MongoDB Atlas - connection - socket import unsaved file added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const socket = require('socket.io');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
